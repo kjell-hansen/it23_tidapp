7b70fd3a7c1d3dc01dd19205fb570089544164bf
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2025-01-28 11:04:56 +0200

    Hämta sammanställning med testcases ok!

--- src/Compilations.php
+++ src/Compilations.php
@@ -9,7 +9,6 @@ declare (strict_types=1);
  * @return Response
  */
 function compilations(Route $route): Response {
-    return new Response("Compilations");
     try {
         if (count($route->getParams()) === 2 && $route->getMethod() === RequestMethod::GET) {
             return hamtaSammanstallning($route->getParams()[0], $route->getParams()[1]);
@@ -28,4 +27,53 @@ function compilations(Route $route): Response {
  * @return Response
  */
 function hamtaSammanstallning(string $from, string $tom): Response {
+    // Kontrollera indata
+    $fromDate = Datetime::createFromFormat('Y-m-d', $from);
+    $tomDate = Datetime::createFromFormat('Y-m-d', $tom);
+
+    $datumKoll = [];
+    if ($fromDate === false) {
+        $datumKoll[] = "Felaktigt angivet från-datum";
+    } elseif ($fromDate->format('Y-m-d') !== $from) {
+        $datumKoll[] = "Felaktigt formaterat från-datum";
+    }
+    if ($tomDate === false) {
+        $datumKoll[] = "Felaktigt angivet till-datum";
+    } elseif ($tomDate->format('Y-m-d') !== $tom) {
+        $datumKoll[] = "Felaktigt formaterat till-datum";
+    }
+    if ($fromDate && $tomDate && $fromDate->format('Y-m-d') > $tomDate->format('Y-m-d')) {
+        $datumKoll[] = "Från-datum ska vara mindre än till-datum";
+    }
+    if (count($datumKoll) > 0) {
+        array_unshift($datumKoll, "Bad request");
+        $retur = new StdClass();
+        $retur->error = $datumKoll;
+        return new Response($retur, 400);
+    }
+
+    // Koppla databas
+    $db = connectDb();
+
+    // Skicka fråga
+    $stmt = $db->prepare("SELECT aktivitetsid, aktivitet, SEC_TO_TIME(SUM(TIME_TO_SEC (tid))) as summa
+            FROM aktiviteter a 
+            INNER JOIN uppgifter u ON aktivitetsid=a.id
+            WHERE datum BETWEEN :from AND :to
+            GROUP BY aktivitetsid");
+    $stmt->execute(['from' => $fromDate->format('Y-m-d'), 'to' => $tomDate->format('Y-m-d')]);
+    $allaRader = $stmt->fetchAll();
+
+    // Returnera svar
+    $retur = [];
+    foreach ($allaRader as $rad) {
+        $post = new StdClass();
+        $post->activityId = $rad['aktivitetsid'];
+        $post->activity = $rad['aktivitet'];
+        $post->time = substr($rad['summa'], 0, -3);
+        $retur[] = $post;
+    }
+    $svar = new stdClass();
+    $svar->tasks = $retur;
+    return new Response($svar, 200);
 }
--- test/TestCompilation.php
+++ test/TestCompilation.php
@@ -16,12 +16,49 @@ function allaCompilationTester(): string {
 
 /**
  * Tester för funktionen hämta en sammmanställning av uppgifter mellan två datum
- * @return string html-sträng med alla resultat för testerna 
+ * @return string html-sträng med alla resultat för testerna
  */
 function test_HamtaSammanstallning(): string {
     $retur = "<h2>test_HamtaSammanstallning</h2>";
     try {
-        $retur .= "<p class='error'>Inga tester implementerade</p>";
+        // Misslyckas med tester av felaktiga datum:
+        // "1", "2024-13-37", "igår"
+        $testDates = ["1", "2024-13-37", "igår"];
+        foreach ($testDates as $date) {
+            $idag = date('Y-m-d');
+            $svar = hamtaSammanstallning($idag, $date);
+            if ($svar->getStatus() === 400) {
+                $retur .= "<p class='ok'>Hämta sammanställning med från=$idag och till=$date misslyckades, som förväntat</p>";
+            } else {
+                $retur .= "<p class='error'>Hämta sammanställning med från=$idag och till=$date misslyckades, status= "
+                    . $svar->getStatus() . " returnerades</p>";
+            }
+            $svar = hamtaSammanstallning($date, $idag);
+            if ($svar->getStatus() === 400) {
+                $retur .= "<p class='ok'>Hämta sammanställning med från=$date och till=$idag misslyckades, som förväntat</p>";
+            } else {
+                $retur .= "<p class='error'>Hämta sammanställning med från=$date och till=$idag misslyckades, status= "
+                    . $svar->getStatus() . " returnerades</p>";
+            }
+        }
+
+        // Misslyckas med från-datum större än till-datum
+        $svar = hamtaSammanstallning('2025-10-10', '2025-01-01');
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Hämta sammanställning med från mindre än till misslyckades, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta sammanställning med från mindre än till misslyckades, status= "
+                . $svar->getStatus() . " returnerades</p>";
+        }
+
+        // Lyckas med att hämta poster med korrekta datum
+        $svar = hamtaSammanstallning('2025-01-01', '2025-10-10');
+        if ($svar->getStatus() === 200) {
+            $retur .= "<p class='ok'>Hämta poster med korrekta datum lyckades</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta poster med korrekta datum misslyckades, status= "
+                . $svar->getStatus() . " returnerades</p>";
+        }
     } catch (Exception $ex) {
         $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
     }
